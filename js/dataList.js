e33950239e061c1ef01e893e3f2159dbdc12d1ce
simonStromae <48977334+simonStromae@users.noreply.github.com>
2022-02-14 13:27:42 +0100

    new change portfolio

---
+++ js/dataList.js
@@ -0,0 +1,88 @@
+projects = [
+    {
+        "title": "Modern Shop",
+        "slug": "modern-shop",
+        "img_intro":"images/modern-shop-intro.jpg",
+        "techno_web": 'WordPress(Woocommerce) ',
+        "desc_min": "Medical Game est une plateforme d'apprentissage des concept de base dans le domaine de la santé.",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!",
+        "img_details":"images/modern-shop.png",
+        "details": [
+            {
+                "name": 'Page présentation',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/modern-shop-1.png'
+            },
+            {
+                "name": 'Page produit',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/modern-shop-2.png'
+            }
+        ],
+        
+    },
+    {
+        "title": "Medical Game",
+        "slug": "medical-game",
+        "img_intro":"images/medicalGame-intro.jpg",
+        "techno_web": 'React JS',
+        "desc_min": "Medical Game est une plateforme d'apprentissage des concept de base dans le domaine de la santé.",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!",
+        "img_details":"images/medicalGame-1.png",
+        "details": [
+            {
+                "name": 'Page de création compte',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/medicalGame.png'
+            },
+            {
+                "name": 'Page de connexion',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/medicalGame-2.png'
+            },
+            {
+                "name": "Page modules d'apprentissage",
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/medicalGame-3.png'
+            },
+            {
+                "name": "Page details module d'apprentissage",
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/medicalGame-4.png'
+            }
+        ],
+        
+    },
+    {
+        "title": "Power H",
+        "slug": "power-h",
+        "img_intro":"images/powerH-intro.jpg",
+        "techno_web": 'React JS',
+        "desc_min": "Medical Game est une plateforme d'apprentissage des concept de base dans le domaine de la santé.",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!",
+        "img_details":"images/powerH-intro.jpg",
+        "details": [
+            {
+                "name": 'Page de connexion',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/powerH.png'
+            },
+            {
+                "name": 'Page d\'accueil',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/powerH-1.png'
+            },
+            {
+                "name": "Page module gestion logements & résidences",
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/powerH-2.png'
+            },
+            {
+                "name": "Page module gestion résidences",
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "img": 'images/powerH-3.png'
+            }
+        ],
+        
+    }
+];
