6496089f929d2b10dd5237b8e6ca181e08bb9a5c
simonStromae <48977334+simonStromae@users.noreply.github.com>
2022-02-15 09:57:46 +0100

    update info

--- js/dataList.js
+++ js/dataList.js
@@ -4,18 +4,18 @@ projects = [
         "slug": "modern-shop",
         "img_intro":"images/modern-shop-intro.jpg",
         "techno_web": 'WordPress(Woocommerce) ',
-        "desc_min": "Medical Game est une plateforme d'apprentissage des concept de base dans le domaine de la santé.",
-        "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!",
+        "desc_min": "C'est une plateforme commerciale spécialisée dans la vente des appareils téléphoniques et informatiques.",
+        "desc": "Elle a été conçu pour permettre à l'entreprise d'avoir un plus grand nombre de client ainsi d'augmenter ses ventes. ",
         "img_details":"images/modern-shop.png",
         "details": [
             {
                 "name": 'Page présentation',
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": 'Cette page présente sommairement les catégories de produit proposées sur la plateforme.',
                 "img": 'images/modern-shop-1.png'
             },
             {
                 "name": 'Page produit',
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": 'Cette page présente les produits par catégorie.',
                 "img": 'images/modern-shop-2.png'
             }
         ],
@@ -26,13 +26,13 @@ projects = [
         "slug": "sofinez",
         "img_intro":"images/sofinez.png",
         "techno_web": 'WordPress',
-        "desc_min": "Medical Game est une plateforme d'apprentissage des concept de base dans le domaine de la santé.",
-        "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!",
+        "desc_min": "C'est une entreprise spécialisée dans la mise en place de solution logiciel et dans la création de sites internet.",
+        "desc": "Cette entreprise répond aux demandes des petites entreprises pour l'optimisation de leurs outils informatique et web et propose des services sur mesure adaptés aux besoin de fonctionnement et de communication.",
         "img_details":"images/sofinez-2.png",
         "details": [
             {
                 "name": 'Page présentation',
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": 'Cette page d\'accueil a été conçue ainsi dans le but de refletter le professionnalisme et la rigueur dont fait face cette entreprise quand il s\'agit de travailler. Elle Présente également de façon sommaire les différents services que propose cette entreprise.',
                 "img": 'images/sofinez.png'
             }
         ],
@@ -44,27 +44,27 @@ projects = [
         "img_intro":"images/medicalGame-intro.jpg",
         "techno_web": 'React JS',
         "desc_min": "Medical Game est une plateforme d'apprentissage des concept de base dans le domaine de la santé.",
-        "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!",
+        "desc": "Cette plateforme a été conçue pour permettre aux utilisateurs (enfants, adolescents, parents) d'apprendre rapidement et facilement des notions dans le domaine de la santé d'où cette facilité de manipulation des composants, cette rapidité à accéder à l'information.",
         "img_details":"images/medicalGame-1.png",
         "details": [
             {
                 "name": 'Page de création compte',
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": 'Cette page utilisée pour la création de compte permet l\'acquisition des informations de l\'utilisateur, nécessaire la proposition du contenu à l\'utilisateur en fonction des informations récupérées.',
                 "img": 'images/medicalGame.png'
             },
             {
                 "name": 'Page de connexion',
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": 'Cette page permet à l\'utilisateur d\'accéder à son espace de travail en renseignant ses informations de connexion valide.',
                 "img": 'images/medicalGame-2.png'
             },
             {
                 "name": "Page modules d'apprentissage",
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": 'Il s\'agit de la page affichant en fonction de certains paramètres de l\'utilisateur, les différents modules d\'apprentissage qu\'il devra parcourir.',
                 "img": 'images/medicalGame-3.png'
             },
             {
                 "name": "Page details module d'apprentissage",
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": 'Il s\'agit de la page présentant le détails du module d\'apprentissage sous forme de vidéo et regroupé en étape, pour donner à l\'utilisateur une impression de progression dans ses compétences. Pour faciliter l\'enseignement une approche par ',
                 "img": 'images/medicalGame-4.png'
             }
         ],
@@ -75,29 +75,44 @@ projects = [
         "slug": "power-h",
         "img_intro":"images/powerH-intro.jpg",
         "techno_web": 'React JS',
-        "desc_min": "Medical Game est une plateforme d'apprentissage des concept de base dans le domaine de la santé.",
-        "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!",
+        "desc_min": "Power H est une plateforme permettant la gestion d'établissement hotélière.",
+        "desc": "",
         "img_details":"images/powerH-intro.jpg",
         "details": [
             {
                 "name": 'Page de connexion',
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": '',
                 "img": 'images/powerH.png'
             },
             {
                 "name": 'Page d\'accueil',
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": '',
                 "img": 'images/powerH-1.png'
             },
             {
                 "name": "Page module gestion logements & résidences",
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": '',
                 "img": 'images/powerH-2.png'
             },
             {
                 "name": "Page module gestion résidences",
-                "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi saepe officia, consectetur praesentium minus reiciendis, accusamus consequuntur iusto ad illum quibusdam commodi facere tenetur. Culpa quia laborum quam, sint laboriosam delectus veniam, at molestiae magnam iusto similique dolorem ea doloremque!',
+                "desc": '',
                 "img": 'images/powerH-3.png'
+            },
+            {
+                "name": "Page module gestion caisses & facturations",
+                "desc": '',
+                "img": 'images/powerH-4.png'
+            },
+            {
+                "name": "Page module gestion caisses",
+                "desc": '',
+                "img": 'images/powerH-5.png'
+            },
+            {
+                "name": "Page module facturation",
+                "desc": '',
+                "img": 'images/powerH-6.png'
             }
         ],
         
